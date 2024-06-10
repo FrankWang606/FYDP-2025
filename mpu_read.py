892f872ceab86e26dab2f743665b463a2ad86bb3
Frank Wang <fx4wang@uwaterloo.ca>
2024-06-10 16:24:41 -0400

    thumb up

--- mpu_read.py
+++ mpu_read.py
@@ -2,14 +2,15 @@ import serial
 from collections import deque
 import threading
 import time
-import keyboard  
+import keyboard  # 导入 keyboard 库
 from mpudata import MPUData 
 
+TEST_FINGER = 0 # 0 thumb  1 forefinger  2 midfinger 
 lock = threading.Lock()
+# pause_event = threading.Event()
+
 current_gesture = -1
-# 创建一个事件对象，用于控制手势识别线程的暂停和继续
-pause_event = threading.Event()
-# 设置串口
+# 设置串口参数
 ser = serial.Serial('COM3', 9600)  # 根据实际情况更改 COM 端口
 
 # 创建三个双端队列
@@ -28,6 +29,7 @@ def parse_data(data):
         return mpu_name, MPUData(ax, ay, az, rx, ry, rz)
     return None, None
 
+# 读取串口数据的函数
 def read_data():
     global running
     while running:
@@ -44,87 +46,83 @@ def read_data():
             elif mpu_name == "MPU2":
                 mpu2_queue.append(mpu_data)
 
+def thumb_up():
+    if len(mpu0_queue) <= 5 or len(mpu1_queue) <= 5:
+        return False
+    # pause_event.wait()
+    dt0 = mpu0_queue
+    dt1 = mpu1_queue
+    res = True
+    for i in range(-4,0):
+        if dt0[i].ax >=7 and dt1[i].ay<=-8 and dt1[i].az<=-3:
+        # if dt[i].ax >=7:
+            continue
+        else:
+            res = False
+            break
+    return res
+
+# 打印队列内容的函数，用于测试
+def print_queues():
+    global current_gesture,running
+    while running:
+        if len(mpu1_queue) == 0:
+            continue
+        data = mpu1_queue[-1]
+        data.printself()
+        if(thumb_up()):
+            with lock:
+                current_gesture = 0
+            time.sleep(3)
 
 # 监听组合键的函数
 def listen_for_exit():
     global running
     keyboard.add_hotkey('q', lambda: set_running_false())
-    
-def thumb_up():
-    global running, current_gesture
-    res = False
-    while running:
-        if len(mpu0_queue) <= 5:
-            continue
-        # pause_event.wait()
-        dt = mpu0_queue[-1]
-        dt.printself()
-        res = True
-        for i in range(-3,0):
-            j=-1
-            # if mpu0_queue[i].ax >=7 and mpu1_queue[i].ay<=-9 and mpu1_queue[i].az<=-4:
-            if mpu0_queue[j].ax >=7:
-                j-=1
-                continue
-            else:
-                res = False
-                break
-        if res:
-            with lock:
-                if current_gesture == -1:
-                    current_gesture = 0
-            
-    
-def gesture_recognizer():
-    global running, current_gesture
-    while running:
-        if current_gesture != -1:
-            if current_gesture == 0:
-                print("thumb up")
-            elif current_gesture == 1:
-                print("thumb down")
-            elif current_gesture == 2:
-                print("wave")
-            elif current_gesture == 3:
-                print("pinch in")
-            elif current_gesture == 4:
-                print("palm flip")
-            elif current_gesture == 5:
-                print("SELFDEFINED") 
-                
-            pause_event.clear()
-            time.sleep(3)
-            current_gesture = -1
-            pause_event.set()   
-        
 
 def set_running_false():
     global running
     running = False
+    
+def current_state_print(state):
+    if state == -1:
+        print("no gesture detected")
+    if state == 0:
+        print("thumb up")
+    elif state == 1:
+        print("thumb down")
+    elif state == 2:
+        print("wave")
+    elif state == 3:
+        print("pinch in")
+    elif state == 4:
+        print("palm flip")
+    elif state == 5:
+        print("SELFDEFINED") 
 
 if __name__ == "__main__":
-    pause_event.set()
     # 启动一个线程读取串口数据
     thread = threading.Thread(target=read_data)
     thread.start()
 
-    # 启动多线程手势识别
-    ges1 = threading.Thread(target=thumb_up)
-    ges1.start()
-    # 启动一个线程输出手势识别结果
-    rec_thread = threading.Thread(target=gesture_recognizer)
-    rec_thread.start()
+    # 启动一个线程打印队列内容
+    print_thread = threading.Thread(target=print_queues)
+    print_thread.start()
 
-
+    # 主线程监听组合键
     listen_for_exit()
 
     while running:
-        time.sleep(0.1)  # 让主线程保持运行状态
+        current_state_print(current_gesture)
+        if(current_gesture != -1):
+            current_gesture = -1
+            time.sleep(3)
+            continue
+        time.sleep(0.2)
 
-
+    # 等待其他线程结束
     thread.join()
-    rec_thread.join()
-    ges1.join()
+    print_thread.join()
     ser.close()
 
     print("Program terminated.")
